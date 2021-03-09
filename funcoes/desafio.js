e79c97b7d45b4f9ad652fbe53302f33a1deb4321
felipeafbruno <felipedobru@gmail.com>
2021-03-09 10:42:53 -0300

    Desafio Retornando Função apartir de outra Função

--- funcoes/desafio.js
+++ funcoes/desafio.js
@@ -1,3 +1,34 @@
 // SOMAR(3)(4)(5)
-// CALCULAR(3)(7)(FN)
+function somar(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c
+        }
+    }
+}
+const resultado = somar(1)(2)(3)
+console.log(resultado)
 
+
+// CALCULAR(3)(7)(FN)
+const multplicacao = function (n1, n2) {
+    return n1 * n2
+}
+const soma = function (n1, n2) {
+    return n1 + n2
+}
+const subtracao = function (n1, n2) {
+    return n1 - n2
+}
+
+function calcular(a) {
+    return function (b) {
+        return function (fn) {
+            return fn(a, b)
+        }
+    }
+}
+
+console.log('Soma ', calcular(1)(2)(soma))
+console.log('Multiplicar ', calcular(2)(5)(multplicacao))
+console.log('Subtrair ', calcular(10)(4)(subtracao))
