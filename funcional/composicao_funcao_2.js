27efd3a938a38a79fd35ea52d13c8259572316c6
felipeafbruno <felipedobru@gmail.com>
2021-03-11 15:52:56 -0300

    Conceitos de Composição de Funções

---
+++ funcional/composicao_funcao_2.js
@@ -0,0 +1,42 @@
+function composicao(...fns) {
+    return function (valor) {
+        return fns.reduce(async (acc, fn) => {
+            if(Promise.resolve(acc) === acc) {
+                return fn(await acc)
+            } else {
+                return (fn(acc))
+            }
+        }, valor)
+    }
+}
+
+function gritar(texto) {
+    return texto.toUpperCase()
+}
+
+function enfatizar(texto) {
+    return `${texto}!!!`
+}
+
+function tornarLento(texto) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(texto.split('').join(' '))
+        }, 3000)
+    })
+}
+
+const exagerado = composicao(
+    gritar,
+    enfatizar,
+    tornarLento
+)
+exagerado('PARA')
+    .then(console.log)
+
+const menosExagerado = composicao(
+    gritar,
+    enfatizar,
+)
+menosExagerado('Cuidado')
+    .then(console.log)
