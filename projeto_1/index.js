4cc27714a9a5526ed59a435046e22329e63a8969
felipeafbruno <felipedobru@gmail.com>
2021-03-11 15:49:50 -0300

    Refatoração utilizando composição de funções

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -8,17 +8,35 @@ const simbolos = [
 ]
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
-fn.lerArquivo(caminho)
-    .then(fn.filtroPorExtensao('.srt'))
-    .then(fn.lerConteudoArquivos) 
-    .then(fn.separarPorLinha) 
-    .then(fn.removerLinhasVazias)
-    .then(fn.removerLinhasComElemento('-->'))
-    .then(fn.removerLinhasComNumero)
-    .then(fn.removerCaracteresEspeciais(simbolos))
-    .then(fn.separarPorPalavras)
-    .then(fn.removerLinhasVazias)
-    .then(fn.removerLinhasComNumero)
-    .then(fn.agruparPalavras)
-    .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
+const execucao =  fn.composicao(
+    fn.lerArquivo,
+    fn.filtroPorExtensao('.srt'),
+    fn.lerConteudoArquivos,
+    fn.separarPorLinha,
+    fn.removerLinhasVazias,
+    fn.removerLinhasComElemento('-->'),
+    fn.removerLinhasComNumero,
+    fn.removerCaracteresEspeciais(simbolos),
+    fn.separarPorPalavras,
+    fn.removerLinhasVazias,
+    fn.removerLinhasComNumero,
+    fn.agruparPalavras,
+    fn.ordenarPorAtributoNumerico('qtde', 'desc'),
+)
+execucao(caminho)
     .then(console.log)
+
+// fn.lerArquivo(caminho)
+//     .then(execucao)
+//     .then(fn.lerConteudoArquivos) 
+//     .then(fn.separarPorLinha) 
+//     .then(fn.removerLinhasVazias)
+//     .then(fn.removerLinhasComElemento('-->'))
+//     .then(fn.removerLinhasComNumero)
+//     .then(fn.removerCaracteresEspeciais(simbolos))
+//     .then(fn.separarPorPalavras)
+//     .then(fn.removerLinhasVazias)
+//     .then(fn.removerLinhasComNumero)
+//     .then(fn.agruparPalavras)
+//     .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
+//     .then(console.log)
